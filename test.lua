62f979ec5cfa97c06fff7c7cfce19fe5f0db97d0
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2011-11-12 21:13:25 +0100

    meh

--- test.lua
+++ test.lua
@@ -6,9 +6,8 @@ e = jiss.engine()
 -- note that a sequence has to know about the engine it plays on..
 seq = jiss.sequence(e)
 
--- seq:clear()
 seq:insert(0.0, jiss.lua_event("e:midi_note_on_(0, 64, 127)"))
-seq:insert(1.0, jiss.lua_event("print(\"hi\"); e:at(0):relocate(0.0)"))
+seq:insert(0.2, jiss.lua_event("e:at(0):relocate(0.0)"))
 
 -- in non-RT context it's safe to set the state like this
 seq.state = jiss.sequence_STARTED
