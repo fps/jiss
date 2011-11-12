52089970cfbbb8cdae5ddf4c5121a7ad777b9947
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2011-11-12 23:02:43 +0100

    randomized velocities

--- test.lua
+++ test.lua
@@ -7,7 +7,7 @@ e = jiss.engine()
 seq = jiss.sequence(e, "s1")
 
 seq:connect("jass:in")
-seq:insert(0.0, jiss.lua_event("e:at(0):midi_note_on_(0, 64, 127)"))
+seq:insert(0.0, jiss.lua_event("e:at(0):midi_note_on_(0, 64, math.random()*127)"))
 seq:insert(0.1, jiss.lua_event("e:at(0):relocate(0.0)"))
 
 -- add the sequence to the engine and toggle its state to STARTED
@@ -18,7 +18,7 @@ e:at(0):start()
 seq2 = jiss.sequence(e, "s2")
 
 seq2:connect("jass:in")
-seq2:insert(0.0, jiss.lua_event("e:at(1):midi_note_on_(0, 84, 127)"))
+seq2:insert(0.0, jiss.lua_event("e:at(1):midi_note_on_(0, 84, math.random()*127)"))
 seq2:insert(0.3, jiss.lua_event("e:at(1):relocate(0.0)"))
 
 -- add the sequence to the engine and toggle its state to STARTED
