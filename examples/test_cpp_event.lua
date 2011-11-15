dada3b8a7d01e0ef84dfbca72039912102239edd
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2011-11-15 21:53:29 +0100

    hmm, whitespace

--- examples/test_cpp_event.lua
+++ examples/test_cpp_event.lua
@@ -7,10 +7,12 @@ e = jiss.engine()
 -- e:exec_cpp_event executes the event in non-RT context, so
 -- one can allocate stuff there, etc..
 -- note that this may only be done when the engine is stopped
-e:exec_cpp_event(cpp_event([[
+e:exec_cpp_event(cpp_event(
+[[
 	e.storage_append<int>(64);
 	std::cout << "doing stuff :D" << std::endl;
-]]))
+]]
+))
 
 s = cpp_seq(e, "rand", lines(1.0, 
 [[
