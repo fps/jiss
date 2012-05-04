aa8668a826d089ed729bc3cab168ef91693527e2
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-05-04 09:42:08 +0200

    newlines in example

--- examples/patterns.lua
+++ examples/patterns.lua
@@ -16,6 +16,8 @@ play(
 	loop(
 		16 * tick, 
 		cpp_seq(e, "s1", format_xs(
-					  tick, p, 'std::cout<< "hi %s" << std::endl;', {x="10", y=20, z=30}))))
+					  tick, p, 
+					  'std::cout<< "hi %s" << std::endl;', 
+					  {x="10", y=20, z=30}))))
 
 e:start()
