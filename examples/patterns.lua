03f92e6dcbb87787bec7dd89f8032af30b269dcb
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-05-04 09:33:36 +0200

    take function to jissing

--- examples/patterns.lua
+++ examples/patterns.lua
@@ -8,24 +8,7 @@ x   x   x   x
     z       z
 ]]
 
-xs = 
-	function(tick, p, f, y) 
-		ret = {}
-		for line in p:gmatch("[^\r\n]+") do
-			for i = 1, #line do
-				if line:sub(i,i) ~= " " then
-					-- print(f(line:sub(i,i), y))
-					table.insert(ret, {(i-1)*tick, f(line:sub(i,i), y)})
-				end
-			end
-		end
-		return ret
-	end
 
-format_xs = 
-	function(tick, p, s, t)
-		return xs(tick, p, function(x,y) return string.format(s, y[x]) end, t)
-	end
 
 tick = e:get_samplerate()/4
 
--- jissing.lua
+++ jissing.lua
@@ -163,3 +163,21 @@ function cc(channel, ctrl, val)
 	s:midi_cc(channel, ctrl, val)
 end
 
+xs = 
+	function(tick, p, f, y) 
+		ret = {}
+		for line in p:gmatch("[^\r\n]+") do
+			for i = 1, #line do
+				if line:sub(i,i) ~= " " then
+					-- print(f(line:sub(i,i), y))
+					table.insert(ret, {(i-1)*tick, f(line:sub(i,i), y)})
+				end
+			end
+		end
+		return ret
+	end
+
+format_xs = 
+	function(tick, p, s, t)
+		return xs(tick, p, function(x,y) return string.format(s, y[x]) end, t)
+	end
