5d89e2f24ec8d2a632cf94c19d71a0376fea646f
Florian Paul Schmidt <fschmidt@techfak.uni-bielefeld.de>
2012-05-04 09:40:17 +0200

    newlines in example

--- examples/patterns.lua
+++ examples/patterns.lua
@@ -12,6 +12,10 @@ x   x   x   x
 
 tick = e:get_samplerate()/4
 
-play(loop(16 * tick, cpp_seq(e, "s1", format_xs(tick, p, 'std::cout<< "hi %s" << std::endl;', {x="10", y=20, z=30}))))
+play(
+	loop(
+		16 * tick, 
+		cpp_seq(e, "s1", format_xs(
+					  tick, p, 'std::cout<< "hi %s" << std::endl;', {x="10", y=20, z=30}))))
 
 e:start()
